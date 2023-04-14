7c718ecb1fcd732051d42196f7d9e77cf0bf46cb
Juarez Geraldo Silva Jr <juarezgeraldosilva@gmail.com>
2023-04-14 15:37:20 -0300

    Ataulização git

--- AmigoAPI/Controllers/AmigoController.cs
+++ AmigoAPI/Controllers/AmigoController.cs
@@ -61,7 +61,7 @@ namespace AmigoAPI.Controllers
                 alteraAmigoDTO.PaisId = incluiAmigoDTO.PaisId;
 
                 _amigoService.AlteraAmigo(alteraAmigoDTO);
-                return RedirectToAction(nameof(SelecionaAmigos));
+                return Ok();
             }
             return new EmptyResult();
         }
